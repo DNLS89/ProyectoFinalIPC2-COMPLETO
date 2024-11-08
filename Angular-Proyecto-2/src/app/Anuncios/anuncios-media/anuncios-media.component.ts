7a0999836cd7ba2803f62efe0f19d053cb01a36b
Dany LS <dadeleon89@gmail.com>
2024-11-07 22:56:28 -0700

    Documentos y Cambios a Anuncios-Media

--- Angular-Proyecto-2/src/app/Anuncios/anuncios-media/anuncios-media.component.ts
+++ Angular-Proyecto-2/src/app/Anuncios/anuncios-media/anuncios-media.component.ts
@@ -1,19 +1,23 @@
 import { NgIf } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Anuncio } from '../../../entities/Anuncio';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-anuncios-media',
   standalone: true,
   imports: [NgIf],
   templateUrl: './anuncios-media.component.html',
-  styleUrl: './anuncios-media.component.css'
+  styleUrl: './anuncios-media.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AnunciosMediaComponent implements OnInit{
 
   hayAnuncioTexto: boolean = false;
   hayAnuncioTextoImagen: boolean = false;
   hayAnuncioVideo: boolean = false;
+
+  constructor(private router: Router) {}
 
 
   ngOnInit(): void {
@@ -22,6 +26,8 @@ export class AnunciosMediaComponent implements OnInit{
   @Input({required: true})  anunciosList!: Anuncio[];
 
   comprobarAnuncios() {
+    console.log("Anuncios");
+    console.log(this.anunciosList);
     for (let anuncio of this.anunciosList) {
       if (anuncio.tipo == "TEXTO") {
         this.hayAnuncioTexto = true;
@@ -34,6 +40,16 @@ export class AnunciosMediaComponent implements OnInit{
     }
   }
 
-  randomMath() { return Math.random() }
+  valorAleatorio !: number;
+
+  randomMath() { 
+    
+
+    this.valorAleatorio = Math.random();
+    if (this.valorAleatorio  < 0.5) {
+      console.log(this.router.url);
+    }
+    return this.valorAleatorio; 
+  }
 
 }
